a1ab48da3c1409910416bf3507c6a2907357412c
HR-Shekhar <mehimanshusss@gmail.com>
2026-06-20 23:56:24 +0530

    learnig schema with bugs and incomplete tables

--- taggy-backend/db/migrations/20260619141628_learning.sql
+++ taggy-backend/db/migrations/20260619141628_learning.sql
@@ -7,19 +7,45 @@ CREATE TABLE skill (
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
+
+CREATE TYPE current_status AS ENUM (
+    'DRAFT',
+    'ACTIVE',
+    'ARCHIVED'
+);
 
 CREATE TABLE roadmap_version (
-        
-)
+    id BIGSERIAL PRIMARY KEY,
+    roadmap_id BIGINT references roadmap(id),
+    version_number INTEGER NOT NULL,
+    status current_status NOT NULL,
+    generated_by VARCHAR(20) NOT NULL,
+    published_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
 
 CREATE TABLE roadmap (
-    id BIGSERIAL PRIMARY KEY DEFAULT,
-    skill_id BIGINT REFERENCES skill(id),
-    current_version_id BIGINT NULL REFERENCES roadmap_version(id),
+    id BIGSERIAL PRIMARY KEY,
+    skill_id BIGINT NOT NULL REFERENCES skill(id),
+    current_version_id BIGINT REFERENCES roadmap_version(id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
+
+CREATE TABLE milestone (
+    id BIGSERIAL PRIMARY KEY
+    roadmap_version_id BIGINT REFERENCES roadmap_version(id) ON DELETE RESTRICT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    estimated_hours INTEGER,
+    order_index INTEGER NOT NULL,
+    difficulty VARCHAR(20),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CONSTRAINT estimated_hours_check CHECK (estimated_hours > 0),
+    CONSTRAINT duplication_check UNIQUE(roadmap_version_id, order_index)
+);
 
 -- +goose Down
 DROP TABLE skill;
